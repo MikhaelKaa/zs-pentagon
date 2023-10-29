d92fda34209f8eeaad4a6c0d3f3c388c51d6e773
MichailKaa <unionfalse@gmail.com>
2023-10-29 11:58:01 +0300

    >> added kr1533kp11.v

---
+++ CPLD/logic/kr1533kp11.v
@@ -0,0 +1,12 @@
+`timescale 1 ns / 10 ps
+module kr1533kp11 #(parameter DELAY_RISE = 1, DELAY_FALL = 1)
+(
+    input [3:0] A,
+    input [3:0] B,
+    input SA,
+    input CS,
+
+    output [3:0] Y
+);
+assign #(DELAY_RISE, DELAY_FALL) Y = (CS)?(4'bz):((SA)?(B):(A));
+endmodule
